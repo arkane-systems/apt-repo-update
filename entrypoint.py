afab1ec11fffaea6c98bdd159c97adbce535394e
Alistair Young <avatar@arkane-systems.net>
2021-01-26 13:52:10 -0600

    Added check updir.

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,12 @@
 import os
+import git
+import gnupg
+import json
 import logging
+import re
+import shutil
+
+from debian.debfile import DebFile
 
 debug = os.environ.get('INPUT_DEBUG', False)
 
@@ -31,15 +38,15 @@ if __name__ == '__main__':
     logging.debug(supported_arch_list)
     logging.debug(supported_distro_list)
 
-    key_public = os.environ.get('INPUT_PUBLIC_KEY')
-    key_private = os.environ.get('INPUT_PRIVATE_KEY')
-    key_passphrase = os.environ.get('INPUT_KEY_PASSPHRASE')
+#    key_public = os.environ.get('INPUT_PUBLIC_KEY')
+#    key_private = os.environ.get('INPUT_PRIVATE_KEY')
+#    key_passphrase = os.environ.get('INPUT_KEY_PASSPHRASE')
 
-    logging.debug(key_public)
-    logging.debug(key_private)
-    logging.debug(key_passphrase)
+#    logging.debug(key_public)
+#    logging.debug(key_private)
+#    logging.debug(key_passphrase)
 
-    logging.info('-- Done parsing input --')
+#    logging.info('-- Done parsing input --')
 
     # Clone repo
 
@@ -60,3 +67,13 @@ if __name__ == '__main__':
     git_refs_name = list(map(lambda x: str(x).split('/')[-1], git_refs))
 
     logging.debug(git_refs_name)
+
+    logging.info('-- Done cloning current Github page --')
+
+    # Process files.
+
+    logging.info('-- Processing updates --')
+
+    # Generate metadata
+    os.chdir(github_slug + '/' + update_folder)
+    logging.debug(os.listdir())
